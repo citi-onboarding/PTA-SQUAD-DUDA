4fec5e03b72f6370347bb6ac4776c5fabdafb721
Lucas Torres <ljat@cin.ufpe.br>
2025-11-21 01:46:16 -0300

    feature/topbar: implementação do componente de topbar

---
+++ client/src/components/TopBar/index.tsx
@@ -0,0 +1,60 @@
+import logoCiti from './logo-citi-topbar.svg';
+import Image from "next/image";
+import Link from 'next/link';
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
+
+export default function TopBar() {
+    return(
+        <div className="flex flex-row justify-between items-center h-[70px] max-w-full min-w-full px-4 md:px-8 bg-white border-b border-[#D9D9D9] gap-4">
+
+            <div className=""> 
+                <Image src={logoCiti} alt="Logo Citi" className="h-10 w-auto" />
+            </div>
+
+            <Tabs defaultValue="atendimento">
+                <TabsList className="bg-transparent p-0 gap-3 md:gap-6 mt-1.5">
+
+                    <TabsTrigger value="atendimento"
+                        className="
+                            rounded-none 
+                            p-0 
+                            pb-1 
+                            text-gray-600 
+                            data-[state=active]:shadow-none 
+                            data-[state=active]:text-black 
+                            data-[state=active]:border-b-2 
+                            data-[state=active]:border-[#50E678]"
+                    >
+                                <Link href="#" className="hover:text-[#7D1AD7] ">Atendimento</Link>
+                    </TabsTrigger>
+
+                    <TabsTrigger value="cadastro"
+                        className="
+                            rounded-none 
+                            p-0 
+                            pb-1 
+                            text-gray-600 
+                            data-[state=active]:shadow-none 
+                            data-[state=active]:text-black 
+                            data-[state=active]:border-b-2 
+                            data-[state=active]:border-[#50E678]"
+                    >
+                                <Link href="#" className="hover:text-[#7D1AD7] ">Cadastro</Link>
+                    </TabsTrigger>
+                </TabsList>
+            </Tabs>
+           
+            <div className="">
+                <p className="text-[#7D1AD7] text-sm font-medium">
+                    Made with <strong>&lt; &#x0002F; &gt;</strong> and{" "} <strong>&hearts;</strong> by CITi
+                </p>
+            </div>  
+
+        </div>
+    )
+}
