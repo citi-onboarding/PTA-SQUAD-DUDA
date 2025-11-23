ae1ea6bb10ed2b79aecb813bcbcce3be5eb5c4c7
dudarmouras <dudarmouras@gmail.com>
2025-11-23 12:45:12 -0300

    fix/buttonChanges: fixed minor issue at width prop

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,20 +1,18 @@
+"use client"
 import Image from "next/image";
 
 import { LogoCITi } from "../assets";
+import { BotaoAcao } from "@/components/Buttons";
 
 export default function Home() {
   return (
     <div className="flex flex-1 flex-col h-full justify-around items-center bg-black">
-      <div>
-        <Image src={LogoCITi} alt="Logo citi" />
-      </div>
-      <div className="flex flex-col justify-center items-center">
-        <h1 className="text-white text-4xl font-bold">NextJS Boilerplate</h1>
-        <p className="text-white text-xl">
-          Made with <strong>&lt; &#x0002F; &gt;</strong> and{" "}
-          <strong>&hearts;</strong> by CITi         
-        </p>
-      </div> 
+        <BotaoAcao
+          texto="Salvar"
+          onClick={() => {}}
+          cor="bg-[#50E678] hover:bg-[#2a9c48ff]"
+          width={88}
+        />
     </div>
   );
 }
