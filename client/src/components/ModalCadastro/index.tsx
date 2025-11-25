da1328c6f3b5a2a33fe347b3d8a97a384e1f70e4
Lucas Torres <ljat@cin.ufpe.br>
2025-11-25 19:41:05 -0300

    fix/ModalCadastro: validação de preenchimento de input e as demais especificações reportadas.

--- client/src/components/ModalCadastro/index.tsx
+++ client/src/components/ModalCadastro/index.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { Button } from "@/components/ui/button"
 import {
   Dialog,
@@ -13,15 +14,37 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { TopBarLogo } from "@/assets";
 import Image from "next/image";
+import {useForm, Controller} from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import {z} from "zod"
+
+const modalCadastroSchema = z.object({
+    email: z.string().min(1, "Insira o email do tutor"),
+})
+type ModalCadastroValues = z.infer<typeof modalCadastroSchema> // tipagem inferida do esquema
 
 export function ModalCadastro() {
+
+  const {register, control, handleSubmit, formState: {errors}} = useForm({
+          resolver: zodResolver(modalCadastroSchema),
+          defaultValues: {
+              email: ""
+          }
+      })
+
+   // função onSubmit de teste para verificar validação
+    const onSubmit = (data: ModalCadastroValues) => {
+        console.log("Formulário válido:", data)
+    }
+
   return (
     <Dialog>
-      <form>
+      
         <DialogTrigger asChild>
           <Button variant="outline">Open Dialog</Button>
         </DialogTrigger>
         <DialogContent className="max-h-[95vh] max-w-[95vw] w-[423px] h-[408px] overflow-y-auto sm:rounded-xl rounded-3xl">
+          <form onSubmit={handleSubmit(onSubmit)}>
           <DialogHeader>
             <div className="flex justify-center mt-[18px]"> 
                 <Image src={TopBarLogo} alt="Logo Citi" className="" />
@@ -35,17 +58,18 @@ export function ModalCadastro() {
                 </div>
             </DialogDescription>
           </DialogHeader>
-          <div className="w-full w-[312px] mx-auto mt-2">
-            <div className="flex flex-col gap-2">
+          <div className="w-full mx-auto mt-2 mb-[29px]">
+            <div className="relative flex flex-col gap-2 mr-[48px] ml-[48px]">
               <Label htmlFor="email" className="text-md">E-mail</Label>
-              <Input id="email" name="email-input" placeholder="Digite aqui..." className="border-black h-[50px]" />
+              <Input id="email" placeholder="Digite aqui..." className="border-black h-[50px]" {...register("email")}/>
+              {errors.email && <p className="absolute -bottom-4 left-0 text-red-500 text-xs">{errors.email.message}</p>}
             </div>
           </div>
-          <DialogFooter className="flex flex-col items-center justify-center sm:justify-center mt-3 mb-4"> 
+          <DialogFooter className="flex flex-col items-center justify-center sm:justify-center mt-3 mb-4 mr-[48px] ml-[48px]"> 
             <Button type="submit" className="w-full max-w-[312px] rounded-3xl bg-[#50E678] hover:bg-[#40b860]">Enviar</Button>
           </DialogFooter>
+          </form>
         </DialogContent>
-      </form>
     </Dialog>
   )
 }
