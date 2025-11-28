d0d1ea64006677df398bdbedca57d634ab730ca7
marcos-kaio <marcosb1@outlook.com>
2025-11-27 23:32:46 -0300

    feat/CrudConsulta: implementação da lógica do controller e rotas de consultas

---
+++ server/src/controllers/AppointmentController.ts
@@ -0,0 +1,60 @@
+import { Request, Response } from "express";
+import { Citi, Crud } from "../global";
+
+class AppointmentController implements Crud {
+    constructor(private readonly citi = new Citi("Consultation")) {}
+    create = async(request:Request, response:Response) => {
+        const { tipo, medico, data, descricao, pacienteId } = request.body;
+
+        const isAnyUndefined = this.citi.areValuesUndefined(
+            tipo,
+            medico,
+            data,
+            descricao, 
+            pacienteId
+        );
+        if(isAnyUndefined) return response.status(400).send();
+
+        const newAppointment = { // dados sendo armazenados de acordo com o nome do modelo do bd
+            type: tipo,
+            doctorName: medico,
+            datetime: data,
+            description: descricao,
+            patientId: Number(pacienteId)
+        };
+        const {httpStatus, message} = await this.citi.insertIntoDatabase(newAppointment);
+
+        return response.status(httpStatus).send({message});
+    };
+
+    getAll = async(request:Request, response:Response) => {
+        const {httpStatus, values} = await this.citi.getAll();
+
+        return response.status(httpStatus).send({values});
+    };
+
+    getById = async(request:Request, response:Response) => {
+        const {id} = request.params;
+        const {httpStatus, value} = await this.citi.findById(id);
+
+        return response.status(httpStatus).send({value});
+    };
+
+    delete = async(request:Request, response:Response) => {
+        const {id} = request.params;
+        const {httpStatus, messageFromDelete} = await this.citi.deleteValue(id);
+
+        return response.status(httpStatus).send({messageFromDelete});
+    };
+
+    update = async(request:Request, response:Response) => {
+        const {id} = request.params;
+        const {object} = request.body;
+        
+        const {httpStatus, messageFromUpdate} = await this.citi.updateValue(id, object);
+
+        return response.status(httpStatus).send({messageFromUpdate});
+    };
+}
+
+export default new AppointmentController();
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import userController from "./controllers/UserController";
+import AppointmentController from "./controllers/AppointmentController";
 
 const routes = express.Router();
 
@@ -7,5 +8,12 @@ routes.post("/user", userController.create);
 routes.get("/user", userController.get);
 routes.delete("/user/:id", userController.delete);
 routes.patch("/user/:id", userController.update);
+
+// Rotas da consulta
+routes.get("/consultas", AppointmentController.getAll); // Lista as consultas
+routes.get("/consultas/:id", AppointmentController.getById); // Busca a consulta por id
+routes.post("/consultas", AppointmentController.create); // Cria uma nova consulta
+routes.put("/consultas/:id", AppointmentController.update); // Atualiza uma consulta existente
+routes.delete("/consultas/:id", AppointmentController.delete); // Deleta uma consulta
 
 export default routes;
