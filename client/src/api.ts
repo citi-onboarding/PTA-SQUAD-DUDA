b3fa83713f776bfb0f1f0b15ad6ba9f2f18ac090
dudarmouras <dudarmouras@gmail.com>
2025-11-28 22:15:35 -0300

    feat/add cors

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://localhost:3001",
+  //baseURL: "https://localhost:3001", quando criar uma branch
+  baseURL: "https://pta-squad-duda.onrender.com/",
 });
 
 export default api;
